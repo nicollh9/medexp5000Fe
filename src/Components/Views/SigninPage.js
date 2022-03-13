bc79ec85480ac7102ae7af9addbe0788ffef602f
nicollh9 <nicollarita06@gmail.com>
2022-03-12 20:37:43 -0600

    react frontend

---
+++ src/Components/Views/SigninPage.js
@@ -0,0 +1,34 @@
+import Signin from './Signin';
+import { useState } from 'react';
+const SigninPage = ()=>{
+  const [txtCorreo, setTxtCorreo] = useState('');
+  const [txtPassword, setTxtPassword] = useState('');
+  const onChangeHandler = ({target: {name, value}})=>{
+    switch (name) {
+      case 'txtCorreo':
+        setTxtCorreo(value);
+        break;
+      case 'txtPassword':
+        setTxtPassword(value);
+        break;
+      default:
+        break;
+    }
+  }
+  // const onChangeHandler = (e) => {
+  //   const {name, value} = e.target;
+  // }
+  return (
+    <>
+      <Signin
+        txtCorreoValue={txtCorreo}
+        txtPasswordValue={txtPassword}
+        onChange={onChangeHandler}
+        errorTxtCorreo=''
+        errorPassword=''
+      />
+    </>
+  )
+}
+
+export default SigninPage;
---
+++ src/Components/Views/Splash.js
@@ -0,0 +1,25 @@
+import Page from "../UX/Page/Page";
+
+const splashStyle={
+    backgroundColor: '#000',
+    color: '#fff',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex:1,
+    flexDirection: 'column',
+    minHeight: '100vh',
+}
+
+const Splash = () =>{
+    return(
+    <Page>
+        <section style={splashStyle}>
+            <h2>Medex V1.0</h2>
+            <h3>Loading ...</h3>
+        </section>
+    </Page>
+    );
+}
+
+export default Splash;
